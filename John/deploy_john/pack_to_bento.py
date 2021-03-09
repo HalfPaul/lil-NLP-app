57c2bfbf2e74f905ad13eb461e52813af51173d2
handertolium <pauliuspusk52@gmail.com>
2021-03-09 14:28:19 +0200

    Deployed John api and made some small fixes

--- John/deploy_john/pack_to_bento.py
+++ John/deploy_john/pack_to_bento.py
@@ -7,7 +7,7 @@ from main import QandA
 def saveBento():
 
     bento_svc = QandA()
-    bento_svc.pack('onnx_model', "./john_model_fp16.onnx")
+    bento_svc.pack('onnx_model', './john_model_fp16.onnx')
     bento_svc.save()
 
 if __name__ == '__main__':
